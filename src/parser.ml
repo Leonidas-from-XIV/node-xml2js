1f77b28cc8229c80b6246647e5d78a7dee88c2a9
Marek Kubica <marek@xivilization.net>
2023-05-26 16:16:12 +0200

    Working conversion from XML to JS objects

--- src/parser.ml
+++ src/parser.ml
@@ -1,7 +1,10 @@
-type tag = string
-type attr = string * string
-type tree = Node of tag * attr list * tree list
+type t = Tag of Xmlm.tag * t list | Text of string
 
 let parse data =
-  let _input = Xmlm.make_input (`String (0, data)) in
-  Node ("root", [ ("hest", "giraf") ], [])
+  let input = Xmlm.make_input (`String (0, data)) in
+  let el tag subtrees = Tag (tag, subtrees) in
+  let data text = Text text in
+  (* use `input_doc_tree` since `make_input gives an
+     (potentially empty) DTD *)
+  let _dtd, doc_tree = Xmlm.input_doc_tree ~el ~data input in
+  doc_tree
--- src/xml2js.ml
+++ src/xml2js.ml
@@ -1,8 +1,34 @@
 module Jsoo = Js_of_ocaml
+module Js = Jsoo.Js
+
+let rec tree_to_js tree =
+  match tree with
+  | Parser.Text s -> Js.Unsafe.inject @@ Js.string s
+  | Tag (tag, subtrees) ->
+      let tag_name, _attrs = tag in
+      let _tag_name_uri, tag_name_local = tag_name in
+      let subtrees =
+        List.map tree_to_js subtrees
+        |> Array.of_list |> Js.array |> Js.Unsafe.inject
+      in
+      Js.Unsafe.obj [| (tag_name_local, subtrees) |]
+
+(* API *)
 
 let () =
-  Jsoo.Js.export "xml2js"
+  Jsoo.Js.export_all
     (object%js
-       method foo () = 42
-       method parseString str _opts _cb = Parser.parse str
+       method parseString str cb =
+         match Parser.parse str with
+         | tree ->
+             let tree = tree_to_js tree in
+             Js.Unsafe.fun_call cb
+               [| Js.Unsafe.inject Js.null; Js.Unsafe.inject @@ Js.some tree |]
+         | exception _ ->
+             Js.Unsafe.fun_call cb
+               [|
+                 Js.Unsafe.inject @@ Js.some "TODO parsing error";
+                 Js.Unsafe.inject Js.null;
+               |]
+       (* method parseString_withOpts str _opts _cb = Parser.parse str *)
     end)
