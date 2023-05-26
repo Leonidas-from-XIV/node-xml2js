d3ff6f4fe4c8e08999c10468ee8373ca284eafd7
Marek Kubica <marek@xivilization.net>
2023-05-26 11:06:01 +0200

    Add formatting

--- src/parser.ml
+++ src/parser.ml
@@ -1,9 +1,7 @@
 type tag = string
 type attr = string * string
-
-type tree =
-  | Node of tag * attr list * tree list
+type tree = Node of tag * attr list * tree list
 
 let parse data =
   let _input = Xmlm.make_input (`String (0, data)) in
-  Node ("root", [("hest", "giraf")], [])
+  Node ("root", [ ("hest", "giraf") ], [])
--- src/xml2js.ml
+++ src/xml2js.ml
@@ -1,8 +1,8 @@
 module Jsoo = Js_of_ocaml
 
-let () = Jsoo.Js.export "xml2js" (object%js
-  method foo () = 42
-
-  method parseString str _opts _cb =
-    Parser.parse str
-end)
+let () =
+  Jsoo.Js.export "xml2js"
+    (object%js
+       method foo () = 42
+       method parseString str _opts _cb = Parser.parse str
+    end)
